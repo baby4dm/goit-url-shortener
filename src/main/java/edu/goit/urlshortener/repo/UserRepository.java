e6bd5ac61556cc1ff28e94e0473d5d558d83e4e2
yurasulima <yurasulima124@gmail.com>
2024-09-01 15:44:42 +0300

    add registration

--- src/main/java/edu/goit/urlshortener/repo/UserRepository.java
+++ src/main/java/edu/goit/urlshortener/repo/UserRepository.java
@@ -3,5 +3,12 @@ package edu.goit.urlshortener.repo;
 import edu.goit.urlshortener.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/edu/goit/urlshortener/service/impl/UserServiceImpl.java
+++ src/main/java/edu/goit/urlshortener/service/impl/UserServiceImpl.java
@@ -1,6 +1,38 @@
 package edu.goit.urlshortener.service.impl;
 
+import edu.goit.urlshortener.model.request.SignupRequest;
+import edu.goit.urlshortener.repo.UserRepository;
+import edu.goit.urlshortener.security.model.User;
 import edu.goit.urlshortener.service.UserService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public String createUser(SignupRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("User with username %s already exists".formatted(request.getUsername())); //TODO add exception
+        }
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+
+        return "User created";
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow();
+    }
 }
